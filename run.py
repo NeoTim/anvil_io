bed2a259e1edf66f5f0b92b32309e87665b8966e
Xufeng Wu <wuxufeng1111@126.com>
2018-07-25 13:15:20 -0700

    modify run.py

--- run.py
+++ run.py
@@ -1,18 +1,13 @@
 # running script to test server architect
 
 from extensions.tinkr_garage.tinkr_garage_room import TinkrGarageRoom
-from core.gate_server_base import GateServerBase
+from extensions.tinkr_garage.tinkr_garage_gate import TinkrGateServer
 
 
 def main():
 
-    class TinkrGateServer(GateServerBase):
-        def __init__(self, room_server_class, bind_addr, server_name):
-            GateServerBase.__init__(self, room_server_class, bind_addr, server_name)
-
-    # rs_class = room_server_base.RoomServerFactory.make_room_server_class(0, 0, 0)
     rs_class = TinkrGarageRoom
-    gs = GateServerBase(rs_class)
+    gs = TinkrGateServer(rs_class, ('0.0.0.0', 10000), 'tinkr_garage_gate')
     gs.start_server()
 
 
