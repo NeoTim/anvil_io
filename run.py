1cec170e4477208497cc2e1fdf004a8076c846eb
Xufeng Wu <wuxufeng1111@126.com>
2018-07-27 15:38:42 -0700

    modify run.py

--- run.py
+++ run.py
@@ -6,8 +6,16 @@ from extensions.tinkr_garage.tinkr_garage_gate import TinkrGateServer
 
 def main():
 
+    import time
+
     rs_class = TinkrGarageRoom
     gs = TinkrGateServer(rs_class, ('0.0.0.0', 10000), 'tinkr_garage_gate')
+
+    gs.create_room(666)
+    gs.room_servers[666].run_command('set_storm_enabling', 0)
+
+    time.sleep(0.5)
+
     gs.start_server()
 
 
